48c7f13a112141619e792aecd956f213254361e1
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-23 09:26:33 +0300

    add functionality-pt1

--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 
+from .cart import Cart
+
+from store.models import Product
+from django.shortcuts import get_object_or_404
+
 
 def cart_summary(request):
 
     return render(request, 'cart/cart-summary.html')
 
-def cart_add():
+def cart_add(request):
 
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'POST':
+
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_product_quantity'))
+
+        product = get_object_or_404(Product, id=product_id)
+        
+        cart.add(product=product, product_qty=product_quantity)
 
 def cart_delete():
 
