fa96d0df38048d8ca7f0e1306eead5e90d7cede7
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-24 13:54:16 +0300

    Update functionlity

--- cart/views.py
+++ cart/views.py
@@ -55,16 +55,21 @@ def cart_delete(request):
 
 
 
-    
+def cart_update(request):
 
+    cart = Cart(request)
 
+    if request.POST.get('action') == 'post':
 
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
 
+        cart.update(product=product_id, qty=product_quantity)
 
+        cart_quantity = cart.__len__()
 
+        cart_total = cart.get_total()
 
+        response = JsonResponse({'qyt':cart_quantity, 'total':cart_total})
 
-
-def cart_update():
-
-    pass
+        return response
